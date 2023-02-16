58f9227ed4812ec5af977f52863736c8a6539574
Remingtonewing <rewing@ufl.edu>
2023-02-16 16:48:11 -0500

    Final 2 things to do before finish:
    Fix removeInorder
    Fix search if a value is not found

--- main.cpp
+++ main.cpp
@@ -6,58 +6,59 @@
 
 
 
-struct Node {
-    std::string name;
-    std::string ID;
-    Node* right;
-    Node* left;
-
-    Node(std::string ID, std::string name) {
-        this->ID = ID;
-        this->name = name;
-    }
-
-    Node() {}
-
-    std::string getId() {
-        return ID;
-    }
-
-    std::string getName() {
-        return name;
-    }
-
-};
-
 class TreeNode {
 
+
 public:
+    struct Node {
+        std::string name;
+        std::string ID;
+        Node *right;
+        Node *left;
 
-    Node *head;
-
-    int findHeavy(Node* node) { //Finds which direction the tree "leans" (Mod 4 pg. 28)
-        if (!node) {
-            return 0;
-        } else {
-            int left = findHeavy(node->left) + 1;
-            int right = findHeavy(node->right) + 1;
-            if (left == right) {
-                return right;
-            }
-            if (right < left) {
-                return left;
-            }
-            if (left < right) {
-                return right;
-            }
+        Node(std::string ID, std::string name) {
+            this->ID = ID;
+            this->name = name;
+            left = nullptr;
+            right = nullptr;
         }
+
+        Node() {
+            name = "";
+            ID = "";
+            left = nullptr;
+            right = nullptr;
+        }
+
+        std::string getId() {
+            return ID;
+        }
+
+        std::string getName() {
+            return name;
+        }
+
+    };
+
+
+    Node *head = nullptr;
+
+    int height(Node *root) //Stepik solution 5.1a
+    {
+        if (!root)
+            return 0;
+        return 1 + std::max(height(root->left), height(root->right));
     }
 
-    int balanceTreeFactor(Node* node) { //Mod 4 Pg. 33
-        if (node == nullptr) {
-            return 0;
-        }
+    int balanceTreeFactor(Node *node) { //Mod 4 Pg. 33
         return height(node->left) - height(node->right);
+    }
+
+    int size(Node *node) {
+        if (node == NULL)
+            return 0;
+        else
+            return (size(node->left) + 1 + size(node->right));
     }
 
 
@@ -65,106 +66,103 @@ public:
         if (!root) {
             std::cout << "unsuccessful" << std::endl;
             return root;
-        }
-        if (head->getId() == ID) {
-            head = nullptr;
-            delete root;
-            std::cout<< "successful" << std::endl;
-            return head;
-        }
-        if (std::stoi(root->getId()) < std::stoi(ID))
-            root->right = Remove(root,ID);
-        if (std::stoi(root->getId()) > std::stoi(ID))
-            root->left = Remove(root->left,ID);
-        if (root->getId() == ID) {
+        } else if (std::stoi(root->getId()) < std::stoi(ID))
+            root->right = Remove(root->right, ID);
+        else if (std::stoi(root->getId()) > std::stoi(ID))
+            root->left = Remove(root->left, ID);
+        else if (root->getId() == ID) {
             if (!root->left && !root->right) {
+                if (root->getId() == head->getId()) {
+                    head = nullptr;
+                }
                 delete root;
-                std::cout<< "successful" << std::endl;
+                std::cout << "successful" << std::endl;
                 return nullptr;
-            } if (root->right == nullptr && root->left != nullptr) {
-                root->ID = root->left->getId();
-                root->name = root->left->getName();
-                root->left = root->left->left;
-                root->right = root->left->right;
-                delete root->left;
-
-                std::cout<< "successful" << std::endl;
-
-                return root;
-            } if (root->left == nullptr && root->right != nullptr) {
-                root->ID = root->right->getId();
-                root->name = root->right->getName();
-                root->left = root->right->left;
-                root->right = root->right->right;
-                delete root->right;
-
-                std::cout<< "successful" << std::endl;
-
-                return root;
-            } if (root->right && root->left) {
-                Node* finder = root->right;
+            }
+            if (root->right == nullptr && root->left != nullptr) {
+                Node *temp = root;
+                root = root->left;
+                delete temp;
+                std::cout << "successful" << std::endl;
+            }
+            if (root->left == nullptr && root->right != nullptr) {
+                Node *temp = root;
+                root = root->right;
+                delete temp;
+                std::cout << "successful" << std::endl;
+            }
+            if (root->right && root->left) {
+                Node *finder = root->right;
                 while (finder->left) {
                     finder = finder->left;
                 }
                 root->ID = finder->ID;
                 root->name = finder->name;
-                root->right = Remove(root->right, root->ID);
-                return root;
+                root->right = Remove(root->right, finder->ID);
             }
         }
     }
 
-    Node *rotateRight(Node* node) { //Taken directly from slides :D
-        Node* grandChild = node->right->left;
-        Node* newParent = node->left;
+    Node *rotateRight(Node *node) { //Taken directly from slides :D
+        Node *grandChild = node->left->right;
+        Node *newParent = node->left;
         newParent->right = node;
-        node->right = grandChild;
+        node->left = grandChild;
         return newParent;
     }
 
-    Node *rotateLeft(Node* node) { //Also taken directly from Mod 4. Slidees :D
-        Node* grandChild = node->right->left;
-        Node* newParent = node->right;
+    Node *rotateLeft(Node *node) { //Also taken directly from Mod 4. Slides :D
+        Node *grandChild = node->right->left;
+        Node *newParent = node->right;
         newParent->left = node;
         node->right = grandChild;
         return newParent;
     }
 
     Node *Rotate(Node *root) {
-
+        if (balanceTreeFactor(root) > 1) {
+            if (balanceTreeFactor(root->left) > 0)
+                root = rotateRight(root);
+            else {
+                root->left = rotateLeft(root->left);
+                root = rotateRight(root);
+            }
+        } else if (balanceTreeFactor(root) < -1) {
+            if (balanceTreeFactor(root->right) > 0) {
+                root->right = rotateRight((root->right));
+                root = rotateLeft(root);
+            } else
+                root = rotateLeft(root);
+        }
+        return root;
     }
 
     Node *IDSearch(Node *root, std::string ID) {
         if (!root) {
             return nullptr;
         }
-        if (root->ID != ID) {
+        if (root->getId() != ID) {
             IDSearch(root->left, ID);
             IDSearch(root->right, ID);
         } else {
             std::cout << root->name << std::endl;
             return root;
         }
-        return nullptr;
+        return root;
     }
 
     Node *nameSearch(Node *root, std::string name) {
+        if (!root) {
+            return nullptr;
+        }
         if (root->getName() == name) {
-            std::cout << "successful" << std::endl;
+            std::cout << root->ID << std::endl;
             return root;
         } else {
             nameSearch(root->left, name);
             nameSearch(root->right, name);
         }
-        std::cout << "unsuccessful" << std::endl;
         return root;
-    }
-
-    int height(Node* root) //Stepik solution 5.1a
-    {
-        if (!root)
-            return 0;
-        return 1 + std::max(height(root->left), height(root->right));
     }
 
 
@@ -177,54 +175,62 @@ public:
         return head;
     }
 
+    Node *setHead(Node *node) {
+        this->head = node;
+    }
 
     Node *Insert(Node *root, Node node) {
         if (root == nullptr) {
             if (head == nullptr) {
                 root = new Node(node.getId(), node.getName());
-                head = root;
+                setHead(root);
                 std::cout << "successful" << std::endl;
-                return head;
+                return root;
             } else {
                 std::cout << "successful" << std::endl;
                 return new Node(node.getId(), node.getName());
             }
         }
-        if (root->getId() == node.getId()) {
+        int tempID = std::stoi(node.ID);
+        int pastID = std::stoi(root->ID);
+        if (pastID == tempID) {
             std::cout << "unsuccessful" << std::endl;
             return root;
-        }
-         if (std::stoi(root->getId()) < std::stoi(node.getId())) {
+        } else if (pastID < tempID) {
             root->right = Insert(root->right, node);
-            //Incorporate rotate function
-            return root;
-        } if (std::stoi(root->getId()) > std::stoi(node.getId())) {
+            root = Rotate(root);
+
+        } else if (pastID > tempID) {
             root->left = Insert(root->left, node);
-             //Incorporate rotate function
-             return root;
-         }
-         std::cout << "unsuccessful" << std::endl;
-         return root;
+            root = Rotate(root);
+        }
+        if (height(root) > height(head)) {
+            setHead(root);
+        }
+        return root;
     }
-    void printPost(Node* node, std::vector<std::string> &printName) { //All three print algorithms present in slides
+
+    void printPost(Node *node, std::vector<std::string> &printName) { //All three print algorithms present in slides
         if (node) {
             printPost(node->left, printName);
             printPost(node->right, printName);
             printName.push_back(node->name);
-        }else {
+        } else {
             std::cout << "";
         }
     }
-    void PrintPre(Node* node, std::vector<std::string> &printName) {
+
+    void PrintPre(Node *node, std::vector<std::string> &printName) {
         if (node) {
             printName.push_back(node->name);
             PrintPre(node->left, printName);
             PrintPre(node->right, printName);
-        }else {
+        } else {
             std::cout << "";
         }
     }
-    void PrintInOrder(Node* node, std::vector<std::string> &printName) {
+
+    void PrintInOrder(Node *node, std::vector<std::string> &printName) {
         if (node) {
             PrintInOrder(node->left, printName);
             printName.push_back(node->name);
@@ -234,16 +240,32 @@ public:
         }
     }
 
+    void printLevelCount(Node *node) {
+        std::cout << height(node) << std::endl;
+    }
+
     void printVector(std::vector<std::string> printName) {
         for (int i = 0; i < printName.size(); i++) {
             if (i != printName.size() - 1)
                 std::cout << printName[i] << ",";
             else
-                std::cout << printName[i];
+                std::cout << printName[i] << std::endl;
         }
     }
 
+    std::vector<std::string> deleteInOrder(Node *root, std::vector<std::string> &nodes) {
+
+        if (nodes.size() != size(root)) {
+            if (root) {
+                deleteInOrder(root->left, nodes);
+                nodes.push_back(root->ID);
+                deleteInOrder(root->right, nodes);
+            }
+        }
+        return nodes;
+    }
 };
+
 
     bool checkNum(std::string userInput) {
         if (userInput.length() != 8) {
@@ -316,24 +338,38 @@ public:
         std::string::size_type commandType = input.find("insert", 0);
         if (commandType != std::string::npos) {
             return "insert";
+        }if (input.substr(0, 12) != "removeInorder") {
+            commandType = input.find("remove", 0);
+            if (commandType != std::string::npos) {
+                return "remove";
+            }
         }
-        commandType = input.find("remove", 0);
+        commandType = input.find("removeInorder", 0);
         if (commandType != std::string::npos) {
-            return "remove";
+            return "removeInorder";
+        }
+        commandType = input.find("printPreorder", 0);
+        if (commandType != std::string::npos) {
+            return "printPreorder";
         }
         commandType = input.find("printInorder", 0);
         if (commandType != std::string::npos) {
             return "printInorder";
         }
-        commandType = input.find("removeInorder", 0);
+        commandType = input.find("printPostorder", 0);
         if (commandType != std::string::npos) {
-            return "removeInorder";
-        }commandType = input.find("search", 0);
+            return "printPostorder";
+        }
+        commandType = input.find("printLevelCount", 0);
+        if (commandType != std::string::npos) {
+            return "printLevelCount";
+        }
+        commandType = input.find("search", 0);
         if (commandType != std::string::npos) {
             return "search";
         }
         return "unsuccessful";
-    } //Needs finishing
+    }
 
 
 
@@ -350,6 +386,7 @@ public:
 
         std::cin >> numLines; //Reads how many commands the user is going to input.
         std::cin.ignore();
+        std::cin.clear();
 
         for (int i = 0; i < numLines; i++) {
             std::getline(std::cin, userInput);
@@ -363,24 +400,89 @@ public:
                     studentID = getIdVal(userInput);
                     studentName = getNameVal(userInput);
 
-                    Node node(studentID, studentName);
+                    TreeNode::Node test = TreeNode::Node(studentID, studentName);
 
-                    root.Insert(root.Head(), node);
+                    root.Insert(root.Head(), test);
 
                 }
-                if (findCommand(userInput) == "remove") {
-                    userInput.erase(0, 6);
+                else if (findCommand(userInput) == "remove" && userInput.substr(0,13) != "removeInorder") {
+                    userInput.erase(0, 7);
+                    studentID = userInput;
+                    root.Remove(root.Head(), studentID);
                 }
-                if (findCommand(userInput) == "printInorder") {
+                else if (findCommand(userInput) == "printInorder") {
+                    if (root.Head() == nullptr) {
+                        std::cout << " " << std::endl;
+                    }
                     root.PrintInOrder(root.Head(), names);
                     root.printVector(names);
-                } if (findCommand(userInput) == "search") {
+                    names.clear();
+                } else if (findCommand(userInput) == "search") {
+                    bool isInt;
+                    TreeNode::Node* result;
                     userInput.erase(0, 7);
-                    Node* result = root.IDSearch(root.Head(), userInput);
-                    if (result == nullptr) {
-                        std::cout << "unsuccessful";
+                    for (char c : userInput) {
+                        if (isdigit(c)) {
+                            isInt = true;
+                        } else {
+                            isInt = false;
+                            break;
+                        }
+                    }
+                    if (!isInt) {
+                        userInput = getNameVal(userInput);
+                        result = root.nameSearch(root.Head(), userInput);
+                        if (result->name != userInput && root.height(result) == root.height(root.Head())) {
+                            std::cout << "unsuccessful" << std::endl;
+                        }
+                    }
+                    else {
+                       result = root.IDSearch(root.Head(), userInput);
+                        if (result->ID != userInput && root.height(result) == root.height(root.Head())) {
+                            std::cout << "unsuccessful" << std::endl;
+                        }
+                    }
+
+                } else if (userInput.substr(0,13) == "removeInorder") {
+                    userInput.erase(0,14);
+                    std::vector<std::string> nodes;
+                    int iter = 0;
+                    nodes = root.deleteInOrder(root.Head(),nodes);
+                    for (int i = 0; i < root.size(root.Head()); i++) {
+                        iter++;
+                        if (i == std::stoi(userInput)) {
+                            std::string tempID = nodes[i];
+                            std:: cout<< tempID;
+                            root.Remove(root.Head(),tempID);
+                            break;
+                        }
+                        if (iter == nodes.size())  {
+                            std::cout << "unsuccessful" << std::endl;
+                        }
+                    }
+                } else if (findCommand(userInput) == "printPostorder") {
+                    if (root.Head() == nullptr) {
+                        std::cout << " " << std::endl;
+                    } else {
+                        root.printPost(root.Head(), names);
+                        root.printVector(names);
+                        names.clear();
                     }
                 }
+                else if (findCommand(userInput) == "printPreorder") {
+                    if (root.Head() == nullptr) {
+                        std::cout << " " << std::endl;
+                    } else {
+                        root.PrintPre(root.Head(), names);
+                        root.printVector(names);
+                        names.clear();
+                    }
+                } else if (findCommand(userInput) == "printLevelCount") {
+                    root.printLevelCount(root.Head());
+                } else {
+                    std::cout << "unsuccessful" << std::endl;
+                }
+
             }
 
         return 0;
